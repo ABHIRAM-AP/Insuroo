1cb0c5476e8530a1cc2baeed09ac2eba1597a5d4
Abhiram <abhiramap05@gmail.com>
2026-03-08 12:33:43 +0530

    changed vector store loading from normal server loading to life span event handler

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 import os
 
-from src.rag import VectorStoreManager,InsuranceRAG
-# from src.rag import DocumentProcessor
-from data.models.qa_model import QuestionRequest,AnswerResponse
+from src.rag import VectorStoreManager, InsuranceRAG
+from data.models.qa_model import QuestionRequest, AnswerResponse
 
 load_dotenv()
-app = FastAPI()
 
-vector_store_manager = VectorStoreManager()
-vector_store = vector_store_manager.load_vectorstore()
-
-print("📦 Vector store count:", vector_store._collection.count())
-
-rag = InsuranceRAG(
-    vectorstore=vector_store,
-    api_key=os.environ['GEMINI_API_KEY']
-)
+rag = None
 
 
-@app.post("/query/ask",response_model=AnswerResponse)
-def ask_question(req:QuestionRequest):
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global rag
 
-    result=rag.query(req.question)
+    vector_store_manager = VectorStoreManager()
+    vector_store = vector_store_manager.load_vectorstore()
 
-    return {
-        "answer":result["answer"]
-    }
+    print("📦 Vector store count:", vector_store._collection.count())
 
+    rag = InsuranceRAG(
+        vectorstore=vector_store,
+        api_key=os.getenv("GEMINI_API_KEY")
+    )
+
+    yield  # app runs here
+
+    print("🛑 Shutting down API")
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/query/ask", response_model=AnswerResponse)
+def ask_question(req: QuestionRequest):
+    result = rag.query(req.question)
+    return {"answer": result["answer"]}
 
 
 @app.get("/health")
 def root():
-    return {
-        "status":"API is running"
-    }
+    return {"status": "API is running"}
